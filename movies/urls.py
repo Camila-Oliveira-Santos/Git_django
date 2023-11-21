11c8c3592b75c38f48596c24cb533e90ff176c3f
camila.o.s <camila.o.s@usp.br>
2023-11-20 21:37:01 -0300

    Configurar permissões e grupos para usuários

--- movies/urls.py
+++ movies/urls.py
@@ -7,8 +7,10 @@ urlpatterns = [
     path('', views.MovieListView.as_view(), name='index'),  # edite esta linha
     path('search/', views.search_movies, name='search'),
     path('create/', views.create_movie, name='create'),
-    path('<int:movie_id>/', views.detail_movie,
-         name='detail'),
+    path('<int:movie_id>/', views.detail_movie, name='detail'),
     path('update/<int:movie_id>/', views.update_movie, name='update'),
     path('delete/<int:movie_id>/', views.delete_movie, name='delete'),
+    path('<int:movie_id>/review/', views.create_review, name='review'),
+    path('lists/', views.ListListView.as_view(), name='lists'),
+    path('lists/create', views.ListCreateView.as_view(), name='create-list'),
 ]
